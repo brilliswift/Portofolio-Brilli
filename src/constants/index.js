b70be4ad23c0b8def3ab1bdfe09e684fa885ff53
brilliswift <iambrillia@gmail.com>
2025-01-25 12:24:16 +0700

    refactor : add contants index.js

---
+++ src/constants/index.js
@@ -0,0 +1,119 @@
+import {
+    profil,
+    project01,
+    project02,
+    project03,
+    project04,
+    project05,
+    project06,
+    html,
+    css,
+    reactjs,
+    expressjs,
+    mysql,
+    postgresql,
+    figma,
+} from '../assets'
+
+export const sitemapLinks = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'About',
+        link: '/about'
+    },
+    {
+        name: 'Project',
+        link: '/project'
+    },
+    {
+        name: 'Experience',
+        link: '/experience'
+    }
+    {
+        name: 'Contact',
+        link: '/contact'
+    },
+];
+
+export const projects = [
+    {
+        id: 'project-1',
+        name: 'Project 1',
+        link: '',
+        image: '',
+        description: ''
+    },
+    {
+        id: 'project-2',
+        name: 'Project 2',
+        link: '',
+        image: '',
+        description: ''
+    },
+    {
+        id: 'project-3',
+        name: 'Project 3',
+        link: '',
+        image: '',
+        description: ''
+    },
+    {
+        id: 'project-4',
+        name: 'Project 4',
+        link: '',
+        image: '',
+        description: ''
+    },
+    {
+        id: 'project-5',
+        name: 'Project 5',
+        link: '',
+        image: '',
+        description: ''
+    },
+    {
+        id: 'project-6',
+        name: 'Project 6',
+        link: '',
+        image: '',
+        description: ''
+    },
+];
+
+export const experiences = [
+    {
+        id: 'experience-1',
+        desc: ''
+    },
+    {
+        id: 'experience-2',
+        desc: ''
+    },
+    {
+        id: 'experience-3',
+        desc: ''
+    },
+    {
+        id: 'experience-4',
+        desc: ''
+    },
+    {
+        id: 'experience-5',
+        desc: ''
+    }
+
+]
+
+export const socialMedia = [
+    {
+        name: 'Github',
+        link: ''
+    },
+    {
+        name: 'Linkedin',
+        link: ''
+    }
+]
