579ac20c210957aa7b847759004ee2ffe10007e8
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-01-30 12:30:31 +0700

    feat : add button

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,23 +1,82 @@
 import React from 'react';
 import styles from '../styles';
+import {css} from "@emotion/css";
+import styled from '@emotion/styled'
+
+
+const color = 'white'
+const Button = styled.button`
+  padding: 3px;
+    height: 40px;
+    width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  background-color: #C5C5C5;
+  font-size: 14px;
+  border-radius: 25px;
+  
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
 
 const About = () => {
     return (
-        <div className="flex flex-col text-center">
-            <p className={`${styles.paragraph}`}>
-                I am a full-stack developer with 2 years of experience
-            </p>
-            <p className={`${styles.paragraph}`}>
-                specializing in front-end and back-end development.
-            </p>
-            <p className={`${styles.paragraph}`}>
-                My expertise lies in crafting robust and scalable SaaS-based
-            </p>
-            <p className={`${styles.paragraph}`}>
-                architectures on the Google Cloud Platform.
-            </p>
-        </div>
-    );
-};
+        <>
+            <div className="flex flex-col text-center">
+                <p className={`${styles.paragraph}`}>
+                    I am a full-stack developer with 2 years of experience
+                </p>
+                <p className={`${styles.paragraph}`}>
+                    specializing in front-end and back-end development.
+                </p>
+                <p className={`${styles.paragraph}`}>
+                    My expertise lies in crafting robust and scalable SaaS-based
+                </p>
+                <p className={`${styles.paragraph}`}>
+                    architectures on the Google Cloud Platform.
+                </p>
+                <div
+                    className={css`
+                       padding: 48px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                       gap: 12px;
+                    `}
+                >
+                    <div
+                        className={css`
+                            margin-top: 2rem;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            color: grey;
 
-export default About;
+
+                        `}
+                    >
+                        <Button>Get in touch</Button>
+                    </div>
+                    <div
+                        className={css`
+                            margin-top: 2rem;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            color: grey;
+
+
+                        `}
+                    >
+                        <Button>Download CV</Button>
+                    </div>
+                </div>
+                </div>
+            </>
+            );
+            };
+
+            export default About;
