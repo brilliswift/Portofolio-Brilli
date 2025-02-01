76436b9a24c6963b4cb0abfbb27df909e3817e4a
brilliswift <iambrillia@gmail.com>
2025-02-01 12:09:28 +0700

    docs : update experiences data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,42 +1,42 @@
-import {
-    profil,
-    project01,
-    project02,
-    project03,
-    project04,
-    project05,
-    project06,
-    html,
-    css,
-    reactjs,
-    expressjs,
-    mysql,
-    postgresql,
-    figma,
-} from '../assets'
+// import {
+//     profil,
+//     project01,
+//     project02,
+//     project03,
+//     project04,
+//     project05,
+//     project06,
+//     html,
+//     css,
+//     reactjs,
+//     expressjs,
+//     mysql,
+//     postgresql,
+//     figma,
+// } from '../assets'
 
-export const sitemapLinks = [
-    {
-        name: 'Home',
-        link: '/'
-    },
-    {
-        name: 'About',
-        link: '/about'
-    },
-    {
-        name: 'Project',
-        link: '/project'
-    },
-    {
-        name: 'Experience',
-        link: '/experience'
-    }
-    {
-        name: 'Contact',
-        link: '/contact'
-    },
-];
+// export const NavbarLinks = [
+//     {
+//         name: 'Home',
+//         link: '/'
+//     },
+//     {
+//         name: 'About',
+//         link: '/about'
+//     },
+//     {
+//         name: 'Project',
+//         link: '/project'
+//     },
+//     {
+//         name: 'Experience',
+//         link: '/experience'
+//     }
+//     {
+//         name: 'Contact',
+//         link: '/contact'
+//     },
+// ];
 
 export const projects = [
     {
@@ -44,68 +44,69 @@ export const projects = [
         name: 'Project 1',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
     {
         id: 'project-2',
         name: 'Project 2',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
     {
         id: 'project-3',
         name: 'Project 3',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
     {
         id: 'project-4',
         name: 'Project 4',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
     {
         id: 'project-5',
         name: 'Project 5',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
     {
         id: 'project-6',
         name: 'Project 6',
         link: '',
         image: '',
-        description: ''
+        description: 'test'
     },
 ];
 
 export const experiences = [
     {
         id: 'experience-1',
-        desc: ''
+        company: 'Perusahaan A',
+        position: 'Software Engineer',
+        duration: '2020 - Sekarang',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
     },
     {
         id: 'experience-2',
-        desc: ''
+        company: 'Perusahaan B',
+        position: 'Web Developer',
+        duration: '2018 - 2020',
+        description: 'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
     },
     {
         id: 'experience-3',
-        desc: ''
+        company: 'Perusahaan C',
+        position: 'IT Support Specialist',
+        duration: '2016 - 2018',
+        description: 'Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
     },
-    {
-        id: 'experience-4',
-        desc: ''
-    },
-    {
-        id: 'experience-5',
-        desc: ''
-    }
-
-]
+    // ... Tambahkan lebih banyak pengalaman jika diperlukan
+];
 
 export const socialMedia = [
     {
