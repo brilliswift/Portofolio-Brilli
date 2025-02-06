ea71731d0a8fdcfaa2e0ae4a94d6b215f5a8a635
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-02-06 11:33:49 +0700

    style : fix heading extra-bold

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => (
             padding: 2rem;
              
         `}>
-            <div className="text-center text-white font-poppins font-extrabold text-5xl">
-                <h2 className={`${styles.heading2} leading-[20px] tracking-tighter`}>
+            <div className="text-center text-white font-poppins text-5xl">
+                <h2 className={`${styles.heading2} leading-[20px] tracking-tighter font-weight: 900`}>
                     I do code and
                     <br />
                     love to projects
                     <span className="mx-2">
             <span
-                className="bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent"
+                className="bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 bg-clip-text text-transparent"
             >
               about it!
             </span>
