a446ba2862ba995b09c2984821cb50603f8599e1
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-02-07 12:09:26 +0700

    feat : add contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Sitemap from "./components/Navbar.jsx";
-import Homepage from "./components/Homepage.jsx";
+import Sitemap from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Contact from "./components/Contact";
 
 const App = () => (
     <BrowserRouter>
         <div className="bg-[#161513] min-h-screen">
             <Sitemap />
             <Homepage />
-
+            <Contact/>
         </div>
     </BrowserRouter>
 );
