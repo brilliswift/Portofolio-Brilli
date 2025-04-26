4a74800437f1dd1552c749180647aab2f037341e
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-04-26 12:19:50 +0700

    feat : Add data desc experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -98,35 +98,35 @@ export const experiences = [
         company: 'Project Manager at Paw Connect',
         position: 'Software Engineer',
         duration: 'Sept 2024 - Now',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
+        description: 'Lead end-to-end product development initiatives throught market research and user interviews, resulting in data-driven feature prioritizion. Implemented Agile Scrum method, managing sprint planning and retrospectives for cross-functional teams. Established project tracking systems using Jira, Trello and Taskade.',
     },
     {
         id: 'experience-2',
         company: 'Web Developer Intern at PT Kinema Systrans Multimedia',
         position: 'Web Developer',
         duration: 'Apr - Jun 2024',
-        description: 'Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
+        description: 'Designed and developed cat adoption matching system based on user preferences and filtering algorithms. Built RESTful APIs using Express.js to handle data flow between database and front-end. Created responsive web interface using React.js and TailwindCSS for optimal user experience. Integrated geolocation features using Leaflet to help users find nearby adoptable cats. Collaborated in an Agile team environment, participating in daily stand-ups and sprint reviews.\n ',
     },
     {
         id: 'experience-3',
         company: 'Project Manager Intern at PT Kinema Systrans Multimedia',
         position: 'Project Manager',
         duration: 'Feb - Apr 2024',
-        description: 'Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
+        description: 'Led multiple cross-functional teams in developing two successful web applications, implemented Agile Scrum methodology across both projects. Established and managed project infrastructure using Trello, Discord, and Figmajam, improving team collaboration and productivity by 40%. Led daily stand-up meetings and sprint retrospectives, maintaining clear communication channels and resolving technical blockers.\n ',
     },
     {
         id: 'experience-4',
         company: 'Front-End Web Developer at KBM Al-Mahdiyyin',
         position: 'Front-End Web Developer',
         duration: 'Sept - Dec 2023',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.',
+        description: 'Led web development project from conception to deployment using Agile methodologies. Managed UI/UX design process using Figma, ensuring alignment with user requirements. Coordinated between design and development teams to ensure timely project delivery. Conducted user interviews and usability testing to gather insights and understand user pain points for the attendance web application project at KBM Al-Mahdiyyin. Established project milestones and KPIs, achieving 90% on-time delivery.\n',
     },
     {
         id: 'experience-5',
         company: 'Cloud Computing Cohort at Bangkit Academy',
         position: 'Cloud Engineer',
         duration: 'Feb - Jun 2023',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis.'
+        description: 'Gained a strong understanding of cloud computing fundamentals, hands-on cloud services GCP, and professional project management in industry. Collaborated with cross-functional teams to integrate ML models with frontend and backend systems to developed and deployed an ML-powered food expiration tracking application using image recognition technology.\n'
     },
 ];
 
