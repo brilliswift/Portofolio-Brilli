dd08513a26695395b41ff18c71e62d72c7f728d3
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-04-28 10:52:26 +0700

    fix : To-do fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Sitemap from "./components/Navbar";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from "./components/Homepage";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
+import Experience from "./components/Experience";
 
 const App = () => (
     <BrowserRouter>
         <div className="bg-[#161513] min-h-screen">
-            <Sitemap />
-            <Homepage />
+            <Navbar/>
+            <Homepage/>
             <Contact/>
+            <Routes>
+                <Route path="/" component={Homepage} />
+                <Route path="/project" component={Project} />
+                <Route path="/experience" component={Experience} />
+                <Route path="/contact" component={Contact} />
+            </Routes>
         </div>
     </BrowserRouter>
 );
