f6c0ec4ab927e472ea98dd513ecd45e2e9559146
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-05-02 09:35:06 +0700

    feat: add Image of Profile

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,23 +4,24 @@ import Hero from "./Hero";
 import About from "./About";
 import Project from "./Project";
 import Experience from "./Experience";
+import { profile } from "../assets/index.js";
 
 
 const Homepage = () => {
     return (
         <>
             <div className={css`
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
+                display: flex;
+                align-items: center;
+                justify-content: center;
             `}>
-            <div className="bg-white rounded-full h-24 w-24 flex items-center justify-center">
-                Profil
+                <div className="bg-gray-300 rounded-full flex items-center justify-center h-28 w-28 shadow-lg shadow-gray-500">
+                    <img src={profile} alt="my profile" className="h-28 scale-110 relative left-4 bottom-2"/>
+                </div>
             </div>
-            </div>
-            <Hero />
-            <About />
-            <Project />
+            <Hero/>
+            <About/>
+            <Project/>
             <Experience/>
         </>
     );
