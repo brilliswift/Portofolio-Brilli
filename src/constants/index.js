c8a88a63811cef574eacff6fef332aa7fcccf5ff
Brilliannissa Zulfa <113734241+brilliswift@users.noreply.github.com>
2025-06-04 10:09:53 +0700

    docs : edit data

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,7 +52,7 @@ import brillilogo from '../assets/brilli-logo.png';
 export const projects = [
     {
         id: 'project-1',
-        name: 'Rarasati',
+        name: 'Paw Connect',
         link: '',
         image: pawconnect1,
         description: 'Paw Connect is a user-friendly website that provides a platform for cat lovers to easily find adoptable cats, connect with other cat enthusiasts, and provide cat care guide. During project my role as a Hacker.'
@@ -83,35 +83,35 @@ export const projects = [
 export const experiences = [
     {
         id: 'experience-1',
-        company: 'Project Manager at Paw Connect',
-        position: 'Software Engineer',
+        company: 'Rarasati',
+        position: 'Technical Project Manager',
         duration: 'Sept 2024 - Now',
         description: 'Lead end-to-end product development initiatives throught market research and user interviews, resulting in data-driven feature prioritizion. Implemented Agile Scrum method, managing sprint planning and retrospectives for cross-functional teams. Established project tracking systems using Jira, Trello and Taskade.',
     },
     {
         id: 'experience-2',
-        company: 'Web Developer Intern at PT Kinema Systrans Multimedia',
-        position: 'Web Developer',
+        company: 'PT Kinema Systrans Multimedia',
+        position: 'Web Developer Intern',
         duration: 'Apr - Jun 2024',
         description: 'Designed and developed cat adoption matching system based on user preferences and filtering algorithms. Built RESTful APIs using Express.js to handle data flow between database and front-end. Created responsive web interface using React.js and TailwindCSS for optimal user experience. Integrated geolocation features using Leaflet to help users find nearby adoptable cats. Collaborated in an Agile team environment, participating in daily stand-ups and sprint reviews.\n ',
     },
     {
         id: 'experience-3',
-        company: 'Project Manager Intern at PT Kinema Systrans Multimedia',
-        position: 'Project Manager',
+        company: 'PT Kinema Systrans Multimedia',
+        position: 'Project Manager Intern',
         duration: 'Feb - Apr 2024',
         description: 'Led multiple cross-functional teams in developing two successful web applications, implemented Agile Scrum methodology across both projects. Established and managed project infrastructure using Trello, Discord, and Figmajam, improving team collaboration and productivity by 40%. Led daily stand-up meetings and sprint retrospectives, maintaining clear communication channels and resolving technical blockers.\n ',
     },
     {
         id: 'experience-4',
-        company: 'Front-End Web Developer at KBM Al-Mahdiyyin',
+        company: 'KBM Al-Mahdiyyin',
         position: 'Front-End Web Developer',
         duration: 'Sept - Dec 2023',
         description: 'Defined project scope and set milestones using project management methodologies. Designed UI/UX for an attendance web application using Figma. Conducted user interviews and usability testing to gather insights and understand user pain points for the attendance web application project at KBM Al-Mahdiyyin. Implemented the designed UI/UX for the web application.\n',
     },
     {
         id: 'experience-5',
-        company: 'Cloud Computing Cohort at Bangkit Academy',
+        company: 'Bangkit Academy',
         position: 'Cloud Engineer',
         duration: 'Feb - Jun 2023',
         description: 'Gained a strong understanding of cloud computing fundamentals, hands-on cloud services GCP, and professional project management in industry. Collaborated with cross-functional teams to integrate ML models with frontend and backend systems to developed and deployed an ML-powered food expiration tracking application using image recognition technology.\n'
